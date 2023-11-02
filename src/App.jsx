037b58f4f522d151f29e084fe875c4713df88adc
Hubert Czaplicki <czaplicki.hubert@gmail.com>
2023-11-02 19:39:53 +0100

    Added TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LogInPage';
-import HomePage from './pages/Home';
-import { Routes, Route, Router, BrowserRouter } from 'react-router-dom';
+import React, { useState } from "react";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LogInPage";
+import HomePage from "./pages/Home";
+import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 export default function App() {
-    const   [isLoggedIn, setIsLoggedIn] = useState(false);
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                {!isLoggedIn && <Route path="/login" element={<LoginPage />} />}
-                {!isLoggedIn && <Route path="/register" element={<RegisterPage />} />}
-            </Routes>
-        </BrowserRouter>
-      );
-    }
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {!isLoggedIn && <Route path="/login" element={<LoginPage />} />}
+        {!isLoggedIn && <Route path="/register" element={<RegisterPage />} />}
+      </Routes>
+    </BrowserRouter>
+  );
+}
