a5acceedcd4b02dd71eba20e0d812e1bc1706df8
Hubert Czaplicki <czaplicki.hubert@gmail.com>
2023-11-05 21:44:17 +0100

    Stage 3.2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+// import { useState } from "react";
+// import supabase from "../utilities/client";
 export default function NavBar() {
+  // function FilterPosts() {
+  //   const [searchQuery, setSearchQuery] = useState("");
+  //   const [filteredPosts, setFilteredPosts] = useState([]);
+  //   const handleSearch = async () => {
+  //     try {
+  //       const { data, error } = await supabase
+  //         .from("Posts")
+  //         .select("*")
+  //         .textSearch("description", searchQuery);
+
+  //       if (error) {
+  //         console.error("Błąd podczas filtrowania postów: ", error);
+  //       } else {
+  //         setFilteredPosts(data);
+  //       }
+  //     } catch (error) {
+  //       console.error("Wystąpił błąd: ", error);
+  //     }
+  //   };
+  // }
   return (
     <div className="m-2 basis-1/2">
       <div className="bg-gray-700 p-2 drop-shadow-xl border-slate-800 border-2 border-opacity-60 w-auto h-13 rounded-full">
         <form className="bg-gray-700 flex justify-between">
           <input
+            // value={searchQuery}
+            // onChange={(e) => setSearchQuery(e.target.value)}
             type="text"
             placeholder="Search"
             size="42"
@@ -12,13 +36,18 @@ export default function NavBar() {
             className="m-2 rounded-xl border-gray-700 border-2 border-opacity-40 bg-zinc-300 basis-5/6"
           />
           <button
-            class=" order-last rounded-full bg-zinc-300 border-2 border-gray-700 border-opacity-40 p-1 hover:bg-indigo-300 hover:border-opacity-100 hover:font-medium "
-            type="submit"
+            // onClick={handleSearch}
+            className=" order-last rounded-full bg-zinc-300 border-2 border-gray-700 border-opacity-40 p-1 hover:bg-indigo-300 hover:border-opacity-100 hover:font-medium "
           >
             Browse
           </button>
         </form>
       </div>
+      {/* <ul>
+        {filteredPosts.map((post) => (
+          <li key={post.id}>{post.description}</li>
+        ))}
+      </ul> */}
     </div>
   );
 }
